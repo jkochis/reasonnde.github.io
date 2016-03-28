b992266086513a0e7a9699d7207d5c7e048ca39e
Jesse Kochis <jessekochis@gmail.com>
2016-03-27 20:37:35 -0400

    Update custom.js

--- thinking/elkus-manfredi/intranet-portal/Synthesis51_files/custom.js
+++ thinking/elkus-manfredi/intranet-portal/Synthesis51_files/custom.js
@@ -1,4 +1,3 @@
-/*
 var portalCarousel = {
     sliding : false,
     currentSlide : 0,
@@ -68,42 +67,49 @@ var portalCarousel = {
         slide.prepend(slideText);
         return slide;
     },
-    init : function() {
-        return this.buildCarousel(this.slideData);
-    },
-    // use static data for slides to get started
-    slideData : [
-        {image : './Synthesis51_files/EMA25D_02_Gallery_E_p_8503JS.jpg', title: 'EMA CELEBRATES ONE YEAR AT DRYDOCK OFFICES', copy: 'Litatqui aut odis res voluptati doluptatur, quibernam fuga. Nam facessit lanim et amus. Parunti ut poratur epudist occulpa cus. Icit eatempor dolupta tiossed modis iliquam qui sum non sequiat quam fuga. Ut officaecti aut exceprepud arunt il est.', link: 'link1'},
-        {image : './Synthesis51_files/Republic_Square_Ped_2Night.jpg', title: 'LOREM CELEBRATES ONE YEAR AT DRYDOCK OFFICES', copy: 'Litatqui aut odis res voluptati doluptatur, quibernam fuga. Nam facessit lanim et amus. Parunti ut poratur epudist occulpa cus. Icit eatempor dolupta tiossed modis iliquam qui sum non sequiat quam fuga. Ut officaecti aut exceprepud arunt il est.', link: 'link2'},
-        {image : './Synthesis51_files/LINQ_28_SpaLounge_A_np_8483AB.jpg', title: 'IPSUM CELEBRATES ONE YEAR AT DRYDOCK OFFICES', copy: 'Litatqui aut odis res voluptati doluptatur, quibernam fuga. Nam facessit lanim et amus. Parunti ut poratur epudist occulpa cus. Icit eatempor dolupta tiossed modis iliquam qui sum non sequiat quam fuga. Ut officaecti aut exceprepud arunt il est.', link: 'link3'},
-        {image : './Synthesis51_files/SPRTSQ_VIEW.jpg', title: 'DOLOR CELEBRATES ONE YEAR AT DRYDOCK OFFICES', copy: 'Litatqui aut odis res voluptati doluptatur, quibernam fuga. Nam facessit lanim et amus. Parunti ut poratur epudist occulpa cus. Icit eatempor dolupta tiossed modis iliquam qui sum non sequiat quam fuga. Ut officaecti aut exceprepud arunt il est.', link: 'link4'}
-    ]
+    init : function(data) {
+        return this.buildCarousel(data);
+    }
 }
 // setup the carousel
 jq18(document).ready(function() {
     // create the carousel element and put the slides in
     var carousel = jq18("<div class='carousel'>");
-    carousel.html(portalCarousel.init());
-    // plunk it at the top of the content box
-    cb = jq18('#contentBox').prepend(carousel);
-    cb.css('margin-top', '0');
-    jq18('.carousel-slide-control:eq(0)').addClass('active');
-    portalCarousel.autoPlay = true;
-    jq18('.carousel-slide:eq(0)').css('z-index', 1);
-    // handle clicks on carousel controls
-    jq18('.carousel-slide-control').on('click', function(e) {
-        jq18('.carousel-slide-control').not(this).removeClass('active');
-        jq18(this).addClass('active');
-        index = jq18('.carousel-slide-control').index(this);
-        portalCarousel.loadSlide(jq18('.carousel-slide:eq('+index+')'));
-        if (!e.isTrigger) {
-            clearInterval(autoplay);
-        }
+    jq18.get("http://reasonn.de/thinking/elkus-manfredi/intranet-portal/Synthesis51_files/slides.json", function(response) {
+        html = portalCarousel.init(response);
+        carousel.html(html);
+    })
+    .done(function(){
+      // plunk it at the top of the content box
+      cb = jq18('#contentBox').prepend(carousel);
+      cb.css('margin-top', '0');
+      jq18('.carousel-slide-control:eq(0)').addClass('active');
+      portalCarousel.autoPlay = true;
+      jq18('.carousel-slide:eq(0)').css('z-index', 1);
+      // handle clicks on carousel controls
+      jq18('.carousel-slide-control').on('click', function(e) {
+          jq18('.carousel-slide-control').not(this).removeClass('active');
+          jq18(this).addClass('active');
+          index = jq18('.carousel-slide-control').index(this);
+          portalCarousel.loadSlide(jq18('.carousel-slide:eq('+index+')'));
+          if (!e.isTrigger) {
+              clearInterval(autoplay);
+              autoplay = false;
+          }
+      });
+      jq18('.carousel-slide-control').on('mouseout', function(){
+          if(!autoplay) {
+            autoplay = setInterval(function() {
+                var $cur = jq18('.carousel-slide-control.active').removeClass('active');
+                var $next = $cur.next().length?$cur.next():jq18('.carousel-slide-control:eq(0)');
+                $next.click();
+            }, 6500);
+          }
+      });
+      autoplay = setInterval(function() {
+          var $cur = jq18('.carousel-slide-control.active').removeClass('active');
+          var $next = $cur.next().length?$cur.next():jq18('.carousel-slide-control:eq(0)');
+          $next.click();
+      }, 6500);
     });
-    autoplay = setInterval(function() {
-        var $cur = jq18('.carousel-slide-control.active').removeClass('active');
-        var $next = $cur.next().length?$cur.next():jq18('.carousel-slide-control:eq(0)');
-        $next.click();
-    }, 6500);
 });
-*/
