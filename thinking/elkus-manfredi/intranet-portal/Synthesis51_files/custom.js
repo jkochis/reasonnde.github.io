8cf593db1ed26b9b3eb93bc793cc7850bce42f94
Jesse Kochis <jessekochis@gmail.com>
2016-04-18 19:26:21 -0400

    carousel styling updates

--- thinking/elkus-manfredi/intranet-portal/Synthesis51_files/custom.js
+++ thinking/elkus-manfredi/intranet-portal/Synthesis51_files/custom.js
@@ -105,7 +105,7 @@ if(document.location.search.indexOf("carousel") > -1 && document.forms[0].action
                 if (e.type === 'arrowClick' || !e.isTrigger) {
                     clearInterval(autoplay);
                     portalCarousel.autoPlay = false;
-                    var pauser = setTimeout(function(){
+                    pauser = setTimeout(function(){
                         clearTimeout(pauser);
                         var $cur = jq18('.carousel-slide-control.active').removeClass('active');
                         var $next = $cur.next().length?$cur.next():jq18('.carousel-slide-control:eq(0)');
