5054365ec19d7639b1594b54c091c99d1c0e5eb3
Jesse Kochis <jessekochis@gmail.com>
2016-04-18 18:48:39 -0400

    carousel styling updates

--- thinking/elkus-manfredi/intranet-portal/Synthesis51_files/custom.js
+++ thinking/elkus-manfredi/intranet-portal/Synthesis51_files/custom.js
@@ -98,7 +98,6 @@ if(document.location.search.indexOf("carousel") > -1 && document.forms[0].action
             jq18('.carousel-slide:eq(0)').css('z-index', 1);
             // handle clicks on carousel controls
             jq18('.carousel-slide-control').on('click arrowClick', function(e) {
-                console.log(e);
                 jq18('.carousel-slide-control').not(this).removeClass('active');
                 jq18(this).addClass('active');
                 index = jq18('.carousel-slide-control').index(this);
@@ -106,8 +105,15 @@ if(document.location.search.indexOf("carousel") > -1 && document.forms[0].action
                 if (e.type === 'arrowClick' || !e.isTrigger) {
                     clearInterval(autoplay);
                     portalCarousel.autoPlay = false;
+                    setInterval(function(){
+                        var $cur = jq18('.carousel-slide-control.active').removeClass('active');
+                        var $next = $cur.next().length?$cur.next():jq18('.carousel-slide-control:eq(0)');
+                        $next.click();
+                    },
+                    15000);
                 }
             });
+            // handle left and right arrows
             jq18(document).keyup(function(e) {
                 var $cur = jq18('.carousel-slide-control.active').removeClass('active');
                 switch (e.which) {
