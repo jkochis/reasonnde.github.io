25768f85f2df0a588dbcdd264c06cb102c086c43
Jesse Kochis <jessekochis@gmail.com>
2016-05-09 20:27:45 -0400

    Update custom-local.js

--- thinking/elkus-manfredi/intranet-portal/Synthesis51_files/custom-local.js
+++ thinking/elkus-manfredi/intranet-portal/Synthesis51_files/custom-local.js
@@ -1,6 +1,6 @@
 window.carouselLoaded = false;
 var placeCarousel = function() {
-    if(document.forms[0].action.indexOf("emme/Default.aspx") > -1 && window.carouselLoaded === false) {
+    if(window.carouselLoaded === false) {
         if (jq18('#DeltaTopNavigation a.selected').text() === 'HomeCurrently selected') {
             var portalCarousel = {
                 currentSlide: 0,
